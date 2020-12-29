5b1b581ff6c1cc296a9d14d0bd245f1776a0ddf6
Alex Peña <attn.alex.p@gmail.com>
2020-12-28 16:14:36 -0800

    Added docker and switched to postgress db

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=64)
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    edited = models.DateField(auto_now=True, auto_now_add=False)
+    created = models.DateField(auto_now=False, auto_now_add=True)
